5fdb40e57fd4d65f14c89a6d8352be222d728933
JavierVillafana <19170704@itculiacan.edu.mx>
2022-11-14 09:06:49 -0700

    Initial Commit

---
+++ ExaIW/ExaIWApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExaIWApp.swift
+//  ExaIW
+//
+//  Created by CCDM31 on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ExaIWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
