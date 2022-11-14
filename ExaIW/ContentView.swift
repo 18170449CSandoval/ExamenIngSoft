5a823af9964051f0dc7288082a9d2724998ac3b1
JavierVillafana <19170704@itculiacan.edu.mx>
2022-11-14 09:57:06 -0700

    Creacion de variables

--- ExaIW/ContentView.swift
+++ ExaIW/ContentView.swift
@@ -6,8 +6,24 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var context
+    @FetchRequest(sortDescriptors: [NSSortDescriptor(keyPath: \Item.id, ascending: true)],animation: .default) var items: FetchedResults<Item>
+           @State private var newPedidoID = ""
+           @State private var newPedidoCliente = ""
+           @State private var newPedidoArticulo = ""
+           @State private var newPedidoFechaEntrega = ""
+           @State private var newPedidoDireccion = ""
+           @State private var newPedidoTotal = ""
+           @State private var newPedidoEstado = ""
+           //Variables para actualizar 
+           @State private var PedidoID = ""
+           @State private var PedidoCliente = ""
+           @State private var PedidoArticulo = ""
+           @State private var PedidoFechaEntrega = ""
+           @State private var PedidoDireccion = ""
     var body: some View {
         Text("Hello")
             .padding()
